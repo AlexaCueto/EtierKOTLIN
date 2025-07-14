8eddcfd940a3318fc55b41884b3d552c76d7911d
weiwenxuan <weiwenxuan59@gmail.com>
2025-07-14 14:22:36 +0800

    add Rental.kt

--- app/src/main/java/com/example/etierkotlin/model/Rental.kt
+++ app/src/main/java/com/example/etierkotlin/model/Rental.kt
@@ -1,5 +1,14 @@
 package com.example.etierkotlin.model
 
 data class Rental(
-
+    val renterId:String,
+    val itemName:String,
+    val category:String,
+    val renterFirstName:String,
+    val renterLastName:String,
+    val rentalDate:String,
+    val returnDate:String,
+    val price:Double,
+    val status:String,
+    val notes:String,
 )
